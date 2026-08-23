cb944229da5fc3833dec5fcd0bad885766498f08
HariAswath <hariaswath07@gmail.com>
2026-08-23 17:23:39 +0530

    fix: auth route is fixed

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -65,10 +65,9 @@ const userSchema  = new Schema(
     },
 );
 
-userSchema.pre("save", async function(next) {
-    if(!this.isModified("password")) return next();
+userSchema.pre("save", async function() {
+    if(!this.isModified("password")) return;
     this.password = await bcrypt.hash(this.password, 10);
-    next()
 });
 
 
@@ -111,3 +110,5 @@ userSchema.methods.generateTemporaryToken = function () {
 };
 
 export const User = mongoose.model("User", userSchema);
+
+
