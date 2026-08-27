ac73b3a1ab4b2cb61a311e49c4a89da7b64ba458
HariAswath <hariaswath07@gmail.com>
2026-08-27 11:09:40 +0530

    fix: Email verification route is fixed

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
-import { registerUser, login, logoutUser } from "../controllers/auth.controllers.js";
+import { registerUser, login, logoutUser, verifyEMail } from "../controllers/auth.controllers.js";
 import { userLoginValidator, userRegisterValidator } from "../validators/index.js";
 import { validate } from "../middleware/validator.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
+//unsecured routes
 const router = Router();
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/verify-email/:verificationToken").get(verifyEMail);
+
+
 
 // Secure routes
 router.route("/logout").post(verifyJWT, logoutUser);
